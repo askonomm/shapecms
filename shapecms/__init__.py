4be1f317043e02d9cf2ac9ab4da2c8b75b91d102
Asko Nõmm <asko@repl.ee>
2023-08-12 19:07:28 +0300

    Fix running it

--- shapecms/__init__.py
+++ shapecms/__init__.py
@@ -17,8 +17,8 @@ class ShapeCMS:
         self.client = Blueprint("client", import_name, template_folder="./templates")
 
         # add built-in routes
-        self.add_core_url("/admin", AdminView(), "admin")
-        self.add_core_url("/admin/login", AdminLoginView(), "admin_login")
+        self._add_core_url("/admin", AdminView(), "admin")
+        self._add_core_url("/admin/login", AdminLoginView(), "admin_login")
 
     def add_secret_key(self, key):
         self.app.secret_key = key
@@ -26,7 +26,7 @@ class ShapeCMS:
     def add_shape(self, shape_instance):
         self.shapes.append(shape_instance)
 
-    def add_core_url(self, rule: str, view_class: PageView, view_name: str):
+    def _add_core_url(self, rule: str, view_class: PageView, view_name: str):
         view_class.set_shapes(self.shapes)
 
         self.app.add_url_rule(rule, view_func=view_class.as_view(view_name))
@@ -36,6 +36,7 @@ class ShapeCMS:
 
         self.client.add_url_rule(rule, view_func=view_class.as_view(view_name))
 
-    def run(self, debug: bool = False):
+    def start(self):
         self.app.register_blueprint(self.client)
-        self.app.run(debug=debug)
+
+        return self.app
